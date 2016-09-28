4c13140acd5234b15b5164caab7dc52d5b60d735
natellus <natellus@yahoo.com>
2016-09-28 09:51:45 +0200

    Add clients migration

---
+++ db/migrate/20160928074357_create_cliens.rb
@@ -0,0 +1,12 @@
+class CreateCliens < ActiveRecord::Migration
+  def change
+    create_table :client do |t|
+      t.text :name
+      t.text :phone
+      t.text :datestamp
+      t.text :barber
+      t.text :color
+      t.timesstamps
+    end
+  end
+end
